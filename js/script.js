d19d3a6069326cfe440ef51435fc1e4b1213ca7b
Bogdan <bodnaruk.work@gmail.com>
2022-02-19 16:32:00 +0200

    lern2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 "user strict";
 
 const numberOfFilms = +prompt("Скільки вільмів ви переглянули", "");
+
+if(numberOfFilms < 10 ) {
+    console.log('Просмотрено довольно мало фильмов');
+}else if(numberOfFilms > 10 && numberOfFilms < 30 ) {
+    console.log('Вы классический зритель');
+}else if( numberOfFilms > 30 ) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 const personalMovieBD = {
     count: numberOfFilms,
@@ -10,9 +34,19 @@ const personalMovieBD = {
     privat: false
 };
 
-const lastFilmMovie = prompt("Останній фільм який ви переглядали", ""),
-      reviewFilmMovie = +prompt("Оцініть фільм");
 
-personalMovieBD.movies[lastFilmMovie] = reviewFilmMovie;
+for( let i = 0; i < 2; i++) {
+
+    let lastFilmMovie = prompt("Останній фільм який ви переглядали", ""),
+          reviewFilmMovie = +prompt("Оцініть фільм");
+
+    while( lastFilmMovie.length > 50 && lastFilmMovie !== null ) {
+        lastFilmMovie = prompt("Ви ввели значення з помилкою. Останній фільм який ви переглядали", "");
+        reviewFilmMovie = +prompt("Оцініть фільм");
+    }
+
+    personalMovieBD.movies[lastFilmMovie] = reviewFilmMovie;
+
+}      
 
 console.log(personalMovieBD);
